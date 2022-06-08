b56c2b31b0243817d5e9b14a32d2255b08410743
Malay <malayacharya21@gmail.com>
2022-06-08 09:43:48 +0530

    URL BASICS

--- discordapp/urls.py
+++ discordapp/urls.py
@@ -1,7 +1,15 @@
-
 from django.contrib import admin
 from django.urls import path
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse("Home Page")
+
+def room(request):
+    return HttpResponse("room")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home),
+    path('room/', room)
 ]
