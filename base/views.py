3c0aa75108301f1004887947b4af414b17c8bb91
Malay <malayacharya21@gmail.com>
2022-06-09 11:07:54 +0530

    Passing data to template from views and using python in template files to access the data

--- base/views.py
+++ base/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from discordapp.settings import ROOT_URLCONF
+
+rooms = [
+    {'id':1, 'name':"room1"},
+    {'id':2, 'name':"room2"},
+    {'id':3, 'name':"room3"},
+]
+
 def home(request):
     return render(request, 'home.html')
 
 def room(request):
-    return render(request, 'room.html')
+    return render(request, 'room.html', {'rooms': rooms})
